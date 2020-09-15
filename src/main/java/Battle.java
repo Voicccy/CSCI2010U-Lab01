95ee36c2e76866e323c919d5062c078a7dc5b06a
Zikun Fu <Voice@192.168.2.87>
2020-09-15 12:00:09 -0400

    Lab01 - Zikun Fu

--- src/main/java/Battle.java
+++ src/main/java/Battle.java
@@ -1,5 +1,75 @@
-public class Battle {
-    
-    // YOUR CODE GOES HERE
 
+public class Battle{
+	
+	// YOUR CODE GOES HERE
+	
+	String name1,name2;
+	float attack1,attack2,defense1,defense2,HP1,HP2;
+	int roundNum=0;
+	//Default Constructor
+	public Battle(){
+		name1="";
+		name2="";
+		attack1=0;
+		attack2=0;
+		defense1=0;
+		defense2=0;
+		HP1=0;
+		HP2=0;
+	}
+	
+	//Constructor
+	public Battle(String n1, float a1, float d1, 
+			float H1, String n2, float a2, float d2, float H2){
+		name1 = n1;
+		name2 = n2;
+		attack1 = a1;
+		attack2 = a2;
+		defense1 = d1;
+		defense2 = d2;
+		HP1 = H1;
+		HP2 = H2;
+	}
+	
+	//Methods
+	String getMonster1Name() {
+		return name1;
+	}
+	String getMonster2Name() {
+		return name2;
+	}
+	float getMonster1HP() {
+		return HP1;
+	}
+	float getMonster2HP() {
+		return HP2;
+	}
+	
+	boolean isMonster1Dead(){
+		if(HP1<=0) {
+			return true;
+		}
+		return false;
+	}
+	boolean isMonster2Dead(){
+		if(HP2<=0) {
+			return true;
+		}
+		return false;
+	}
+	
+	void simulateRound() {
+		roundNum++;
+		System.out.printf("Round " + roundNum + "\n");
+		float damage1 = attack1 - defense2;
+		float damage2 = attack2 - defense1;
+		System.out.printf("Chikapu does " + damage1 + " points of damage to Zarichard\n" + 
+				"Zarichard does " + damage2 + " points of damage to Chikapu\n");
+		HP1 = HP1 - damage2;
+		HP2 = HP2 - damage1;
+		System.out.printf("Chikapu:  " + HP1 + "\n" + 
+				"Zarichard:  " + HP2 + "\n");
+	}
+		
+	
 }
--- src/main/java/TaylorSeries.java
+++ src/main/java/TaylorSeries.java
@@ -1,5 +1,30 @@
 public class TaylorSeries {
-
-    // YOUR CODE GOES HERE
-
+	
+	// YOUR CODE GOES HERE
+	
+	static int factorial(int n){
+		if(n>=1) {
+			return n*factorial(n-1);
+		}
+		return 1;
+	}
+	static int pow(int x,int y){
+		if(y>=1) {
+			return x*pow(x,y-1);
+		}
+		return 1;
+	}
+	
+	static float approximateExp(int base, int num) {
+		int count=0;
+		float approx=0;
+		while(count<num) {
+			float top=pow(base,count);
+			float bot=factorial(count);
+			float fraction = top/bot;
+			approx = approx + fraction;
+			count++;
+		}
+		return approx;
+	}
 }
